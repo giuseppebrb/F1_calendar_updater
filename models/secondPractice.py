1b6931e3be285f845932d65e93985149d7c27fc8
Giuseppe Barbato <giuseppe95barbato@gmail.com>
2024-09-22 13:59:28 +0200

    Improved second practice model

--- models/secondPractice.py
+++ models/secondPractice.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional
 
 
 @dataclass
 class SecondPractice:
-    date: str
-    time: str
+    date: Optional[str]
+    time: Optional[str]
 
     @staticmethod
     def from_dict(obj: Any) -> 'SecondPractice':
+        if not obj:
+            return SecondPractice(None, None)
         _date = str(obj.get("date"))
         _time = str(obj.get("time"))
         return SecondPractice(_date, _time)
